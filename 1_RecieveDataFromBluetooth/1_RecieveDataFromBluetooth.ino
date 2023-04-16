af101f29c69950d086136aea8140e73ece98cdca
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-04-16 15:15:05 +0200

    Add files via upload

--- 1_RecieveDataFromBluetooth/1_RecieveDataFromBluetooth.ino
+++ 1_RecieveDataFromBluetooth/1_RecieveDataFromBluetooth.ino
@@ -1,30 +1,29 @@
 #include "BluetoothSerial.h"
 
-/* Check if Bluetooth configurations are enabled in the SDK */
-/* If not, then you have to recompile the SDK */
+
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+
+char recData;
 BluetoothSerial SerialBT;
 
-void setup() {
-  Serial.begin(9600);s
-  /* If no name is given, default 'ESP32' is applied */
-  /* If you want to give your own name to ESP32 Bluetooth device, then */
-  /* specify the name as an argument SerialBT.begin("myESP32Bluetooth*/
-  SerialBT.begin();
+
+void setup() 
+{
+  Serial.begin(9600);
   Serial.println("Bluetooth Started! Ready to pair...");
+  SerialBT.begin();
 }
 
-void loop() {
-  if (Serial.available())
+
+void loop() 
+{
+  recData = SerialBT.read();    
+  if (SerialBT.available()) 
   {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available())
-  {
-    Serial.write(SerialBT.read());
+      Serial.write(recData);
   }
   delay(10);
 }
