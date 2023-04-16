c70f578f78e758592f9f8b5e79a67892f2f66731
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-04-16 16:15:57 +0200

    Add files via upload

--- 1_RecieveDataFromBluetooth/1_RecieveDataFromBluetooth.ino
+++ 1_RecieveDataFromBluetooth/1_RecieveDataFromBluetooth.ino
@@ -1,19 +1,21 @@
 #include "BluetoothSerial.h"
+#include <HardwareSerial.h>
 
 String device_name = "ESP-NAGY";
+char recData;
+BluetoothSerial SerialBT;
+
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-char recData;
-BluetoothSerial SerialBT;
-
 
 void setup() 
 {
-  Serial.begin(9600);
-  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  Serial.begin(9600 , SERIAL_8N1 , 16, 17);
+  delay(100);
+  Serial.printf("\n\nThe device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
   SerialBT.begin(device_name);
 }
 
@@ -23,7 +25,6 @@ void loop()
   recData = SerialBT.read();    
   if (SerialBT.available()) 
   {
-      Serial.write(recData);
+      Serial.println(recData);
   }
-  delay(10);
 }
