6cc8aa86d24ec22e0c451ea5c444a7de1188e665
Mohamed Nagy <106141020+Ged0oo@users.noreply.github.com>
2023-04-19 00:43:37 +0200

    Add files via upload

--- final/final.ino
+++ final/final.ino
@@ -1,56 +1,28 @@
-
 #include <esp_now.h>
 #include <WiFi.h>
 
-//Define variables to store incoming readings
-char receive_rnd_val_1;
+char rec_val;
 
-
-//Structure example to receive data and Must match the sender structure
-typedef struct struct_message 
-{
-    char b;
-} struct_message;
-//Create a struct_message to receive data.
-struct_message receive_Data; 
-
-
-
-//Callback when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) 
 {
-  memcpy(&receive_Data, incomingData, sizeof(receive_Data));
-  //Serial.println();
-  receive_rnd_val_1 = receive_Data.b;
-  //Serial.println("Receive Data:        ");
-  //Serial.println(receive_rnd_val_1);
+  memcpy(&rec_val, incomingData, sizeof(rec_val));
 }
-
-
 
 void setup() 
 {
   Serial.begin(9600);
-  //Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA); 
-
-  //Init ESP-NOW
   if (esp_now_init() != ESP_OK) 
   {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-
-  //Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv); 
+  delay(500);
 }
-
-
 
 void loop() 
 {
-  
-  Serial.write(receive_rnd_val_1);
-  Serial.println("UART REC : ");
-  Serial.println(receive_rnd_val_1);
+  Serial.print(rec_val);
+  Serial.println("\nUART REC : ");
 }
